387d2723ed6f53ebc78f34d574c520422adf973f
Ramiro Peidro <rpeidro@fi.uba.ar>
2021-05-10 17:20:49 -0300

    Agrego el SASS

---
+++ main.js
@@ -0,0 +1,17 @@
+ScrollReveal().reveal('.services', {
+    duration:1800,
+    distance: '50px',
+    reset: true
+});
+
+ScrollReveal().reveal('.items2', {
+    duration:1800,
+    distance: '50px',
+    reset: true
+});
+
+ScrollReveal().reveal('.ubicacion', {
+    duration:1800,
+    distance: '50px',
+    reset: true
+});
--- script.js
+++ script.js
@@ -12,22 +12,3 @@ ham.addEventListener('click', () =>{
     body.classList.toggle('fixed');
 });
 
-/*
-let ham = document.querySelector('.ham');
-let enlaces = document.querySelector('.links');
-let hero = document.querySelector('.heroText');
-let nav = document.querySelector('.navegacion');
-let body = document.querySelector('body');
-
-ham.addEventListener('click', function() {
-    enlaces.classList.toggle('activado');
-    body.classList.toggle('fixed');
-    hero.classList.toggle('oculto');
-});
-
-ham.addEventListener("click", () => {
-    enlaces.classList.toggle('activado');
-    body.classList.toggle('fixed');
-    hero.classList.toggle('oculto');
-})
-*/
