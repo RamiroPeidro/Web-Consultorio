b2660682721e2d58debb539d05f3171578e107e9
Ramiro Peidro <rpeidro@fi.uba.ar>
2021-05-15 12:11:49 -0300

    Agrego SCSS

---
+++ js/main.js
@@ -0,0 +1,41 @@
+ScrollReveal().reveal('.servicesContainer__services', {
+    duration:1800,
+    distance: '50px',
+    reset: true
+});
+
+ScrollReveal().reveal('.servicesContainer__sevices__items2', {
+    duration:1800,
+    distance: '50px',
+    reset: true
+});
+
+ScrollReveal().reveal('.ubicacion', {
+    duration:1800,
+    distance: '50px',
+    reset: true
+});
+
+ScrollReveal().reveal('.mapa__wrapper--contenedor1', {
+    duration:1800,
+    distance: '50px',
+    reset: true
+});
+
+ScrollReveal().reveal('.mapa__wrapper--contenedor2', {
+    duration:2800,
+    distance: '50px',
+    reset: true
+});
+
+ScrollReveal().reveal('.mapa__wrapper__contendor3--subcontenedor1', {
+    duration:2200,
+    distance: '50px',
+    reset: true
+});
+
+ScrollReveal().reveal('.mapa__wrapper__contendor3--subcontenedor2', {
+    duration:2200,
+    distance: '50px',
+    reset: true
+});
