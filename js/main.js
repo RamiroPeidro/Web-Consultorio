f6ecc63612ee7870aeabceb75b7d573373f33c41
Ramiro Peidro <rpeidro@fi.uba.ar>
2021-05-22 20:51:45 -0300

    animaciones en about-us

--- js/main.js
+++ js/main.js
@@ -39,3 +39,42 @@ ScrollReveal().reveal('.mapa__wrapper--subcontenedor2', {
     distance: '50px',
     reset: true
 });
+
+
+ScrollReveal().reveal('.about-us__img', {
+    duration:2200,
+    distance: '90px',
+    reset: true,
+    scale: .5,
+    origin: top
+});
+
+ScrollReveal().reveal('.about-us__titulo-h1', {
+    duration:2200,
+    distance: '90px',
+    reset: true,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.about-us__titulo-p', {
+    duration:3200,
+    distance: '90px',
+    reset: true,
+    origin: 'right'
+});
+
+ScrollReveal().reveal('.info__img', {
+    duration:2200,
+    distance: '90px',
+    reset: true,
+    origin: 'bottom',
+    
+});
+
+ScrollReveal().reveal('.info__img-smaller', {
+    duration:1200,
+    distance: '10px',
+    reset: true,
+    origin: 'bottom',
+    
+});
